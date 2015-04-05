18fa23d422c6f02331252d48c5578ec73d9419b5
Willem <willem66745@gmail.com>
2015-04-05 23:08:26 +0200

    schedule_printer: use real contexts

--- examples/schedule_printer.rs
+++ examples/schedule_printer.rs
@@ -7,7 +7,17 @@ use time::{Timespec, at_utc, Duration};
 const ON: ScheduleContext = ScheduleContext(0);
 const OFF: ScheduleContext = ScheduleContext(1);
 
-struct PrintAction;
+struct PrintAction {
+    id: String
+}
+
+impl PrintAction {
+    fn new(name: &str) -> PrintAction {
+        PrintAction {
+            id: name.to_string()
+        }
+    }
+}
 
 impl ScheduleAction for PrintAction {
     fn kick(&self, timestamp: &Timespec, event: &ScheduleMoment, context: &ScheduleContext) {
@@ -16,25 +26,28 @@ impl ScheduleAction for PrintAction {
             &OFF => "off:",
             _ => unreachable!()
         };
-        println!("{:5}{} {}", action, at_utc(*timestamp).rfc822(), event);
+        println!("{} {:5}{} {}", self.id, action, at_utc(*timestamp).rfc822(), event);
     }
 }
 
 fn main() {
-    let action_handler = PrintAction;
+    let action_handler_1 = PrintAction::new("1");
+    let action_handler_2 = PrintAction::new("2");
+    let action_handler_3 = PrintAction::new("3");
+    let action_handler_4 = PrintAction::new("4");
 
     let mut schedule = Schedule::new().unwrap();
 
     schedule.add_event(ScheduleMoment::Fuzzy(ScheduleTime::new(10,0,0),
-                                             ScheduleTime::new(11,0,0)), &action_handler, ON);
+                                             ScheduleTime::new(11,0,0)), &action_handler_1, ON);
     schedule.add_event(ScheduleMoment::Fuzzy(ScheduleTime::new(12,0,0),
-                                             ScheduleTime::new(13,0,0)), &action_handler, OFF);
-    schedule.add_event(ScheduleMoment::ByClosure(Box::new(|_| ScheduleTime::new(14,0,0)), Duration::minutes(1)), &action_handler, ON);
-    schedule.add_event(ScheduleMoment::ByClosure(Box::new(|_| ScheduleTime::new(15,0,0)), Duration::minutes(1)), &action_handler, OFF);
-    schedule.add_event(ScheduleMoment::Fixed(ScheduleTime::new(19,0,0)), &action_handler, ON);
-    schedule.add_event(ScheduleMoment::Fixed(ScheduleTime::new(21,0,0)), &action_handler, OFF);
-    schedule.add_event(ScheduleMoment::Fixed(ScheduleTime::new(22,0,0)), &action_handler, ON);
-    schedule.add_event(ScheduleMoment::Fixed(ScheduleTime::new(23,0,0)), &action_handler, OFF);
+                                             ScheduleTime::new(13,0,0)), &action_handler_1, OFF);
+    schedule.add_event(ScheduleMoment::ByClosure(Box::new(|_| ScheduleTime::new(14,0,0)), Duration::minutes(1)), &action_handler_2, ON);
+    schedule.add_event(ScheduleMoment::ByClosure(Box::new(|_| ScheduleTime::new(15,0,0)), Duration::minutes(1)), &action_handler_2, OFF);
+    schedule.add_event(ScheduleMoment::Fixed(ScheduleTime::new(19,0,0)), &action_handler_3, ON);
+    schedule.add_event(ScheduleMoment::Fixed(ScheduleTime::new(21,0,0)), &action_handler_3, OFF);
+    schedule.add_event(ScheduleMoment::Fixed(ScheduleTime::new(22,0,0)), &action_handler_4, ON);
+    schedule.add_event(ScheduleMoment::Fixed(ScheduleTime::new(23,0,0)), &action_handler_4, OFF);
 
     schedule.update_schedule(Timespec{sec:0, nsec:0});
     schedule.update_schedule(Timespec{sec:24*3600, nsec:0});
